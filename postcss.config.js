c04bea3987e9e091bb1cffa776e58f0b65b11623
andrew <andrew@93digital.co.uk>
2021-03-26 09:33:47 +0000

    Update scripts and packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
 /* eslint-disable */
 module.exports = {
   plugins: [
-    require('@fullhuman/postcss-purgecss')({
-      content: [ '*.js', '*.php', '*.html' ],
-      keyframes: true,
-      fontFace: true,
-      variables: true,
-    }),
     require('postcss-preset-env')({ stage: 0 }),
     require('cssnano'),
-    require('postcss-pxtorem')({
-      mediaQuery: true,
-    }),
   ],
 };
-
